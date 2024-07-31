dae2800e18ec52dbf6c6173d0c78b1da102f296b
katyadess <145976398+katyadess@users.noreply.github.com>
2024-07-31 23:09:47 +0300

    Update settings.py

--- web41/settings.py
+++ web41/settings.py
@@ -45,9 +45,7 @@ INSTALLED_APPS = [
     'orders',
 ]
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
-CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
