5c356b138ab3fa0dec130df905297480995c1ff3
katyadess <145976398+katyadess@users.noreply.github.com>
2024-08-01 22:14:40 +0300

    Update urls.py

--- web41/urls.py
+++ web41/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.views.static import serve
 from django.conf import settings
+from django.urls import re_path
+
 
 urlpatterns = [
     path('', include('blog.urls')),
@@ -26,8 +29,10 @@ urlpatterns = [
     path('cart/', include(('cart.urls', 'cart'), namespace='cart')),
     path('orders/', include(('orders.urls', 'orders'), namespace='orders')),
     path('shop/', include(('shop.urls','shop'), namespace='shop')),
-] 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
  
